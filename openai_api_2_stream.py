990a465706abe3b706ae77677c85003fdd152d08
DESKTOP-T7DNJQO\doocu <doocubii@gmail.com>
2023-10-05 02:55:15 +0900

    stream 버전

--- openai_api_2_stream.py
+++ openai_api_2_stream.py
@@ -29,7 +29,8 @@ def query_prompt():
     max_tokens=256,
     top_p=1,
     frequency_penalty=0,
-    presence_penalty=0
+    presence_penalty=0,
+    stream=True
   )
 
   return response
@@ -42,10 +43,27 @@ if __name__ == '__main__':
 
   response = query_prompt()
 
-  print(response.choices[0].message["content"])
+  # create variables to collect the stream of chunks
+  collected_chunks = []
+  collected_messages = []
+  # iterate through the stream of events
+  for chunk in response:
+      chunk_time = time.time() - start_time  # calculate the time delay of the chunk
+      collected_chunks.append(chunk)  # save the event response
+      chunk_message = chunk['choices'][0]['delta']  # extract the message
+      collected_messages.append(chunk_message)  # save the message
+      print(f"Message received {chunk_time:.2f} seconds after request: {chunk_message.get('content', '')}")  # print the delay and text
+
+  # print the time delay and text received
+  print(f"Full response received {chunk_time:.2f} seconds after request")
+  full_reply_content = ''.join([m.get('content', '') for m in collected_messages])
+  print(f"Full conversation received: {full_reply_content}")
 
   # 프로그램 종료 시간 기록
   end_time = time.time()
   # 실행 시간 계산
   execution_time = end_time - start_time
   print(f"프로그램 종료: {execution_time} 초")
+
+
+

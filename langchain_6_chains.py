10b390af885c99e60e9d0d76732c0e649703deec
DESKTOP-T7DNJQO\doocu <doocubii@gmail.com>
2023-10-18 10:38:27 +0900

    loader 추가

--- langchain_6_chains.py
+++ langchain_6_chains.py
@@ -20,123 +20,128 @@ llm_model = "gpt-3.5-turbo"
 
 load_dotenv()
 
-
-
 import pandas as pd
-df = pd.read_csv('data/Data.csv')
-
-print(df.head())
-
 
 # ## LLMChain
-
 
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 
+def test_LLMChain() -> None:
+  llm = ChatOpenAI(temperature=0.9, model=llm_model)
 
-llm = ChatOpenAI(temperature=0.9, model=llm_model)
+  prompt = ChatPromptTemplate.from_template(
+    "'{product}'을 만드는 회사를 나타내는 최고로 좋은 이름 하나를 추천하라"
+  )
 
-prompt = ChatPromptTemplate.from_template(
-  "'{product}'을 만드는 회사를 나타내는 최고로 좋은 이름 하나를 추천하라"
-)
+  chain = LLMChain(llm=llm, prompt=prompt)
 
-chain = LLMChain(llm=llm, prompt=prompt)
+  global product
+  product = "퀸 사이즈 침대 시트 세트"
+  print(chain.run(product))
+  return None
 
-product = "퀸 사이즈 침대 시트 세트"
-print(chain.run(product))
 
 
 # ## SimpleSequentialChain
 
 from langchain.chains import SimpleSequentialChain
 
-llm = ChatOpenAI(temperature=0.9, model=llm_model)
+def test_SimpleSequentialChain() -> None:
+  llm = ChatOpenAI(temperature=0.9, model=llm_model)
 
-# prompt template 1
-first_prompt = ChatPromptTemplate.from_template(
-   "'{product}'을 만드는 회사를 나타내는 최고로 좋은 이름 하나를 추천하라"
-)
+  # prompt template 1
+  first_prompt = ChatPromptTemplate.from_template(
+    "'{product}'을 만드는 회사를 나타내는 최고로 좋은 이름 하나를 추천하라"
+  )
 
-# Chain 1
-chain_one = LLMChain(llm=llm, prompt=first_prompt)
+  # Chain 1
+  chain_one = LLMChain(llm=llm, prompt=first_prompt)
 
-# prompt template 2
-second_prompt = ChatPromptTemplate.from_template(
-  "다음 회사를 20단어 이내로 표현하라:{company_name}"
-)
-# chain 2
-chain_two = LLMChain(llm=llm, prompt=second_prompt)
+  # prompt template 2
+  second_prompt = ChatPromptTemplate.from_template(
+    "다음 회사를 20단어 이내로 표현하라:{company_name}"
+  )
+  # chain 2
+  chain_two = LLMChain(llm=llm, prompt=second_prompt)
 
 
-overall_simple_chain = SimpleSequentialChain(chains=[chain_one, chain_two],
-                       verbose=True
-                      )
+  overall_simple_chain = SimpleSequentialChain(chains=[chain_one, chain_two],
+                        verbose=True
+                        )
 
-response = overall_simple_chain.run(product)
-print(response)
-
+  response = overall_simple_chain.run(product)
+  print(response)
+  return None
 
 
 # ## SequentialChain
 
 from langchain.chains import SequentialChain
 
-llm = ChatOpenAI(temperature=0.9, model=llm_model)
+def test_SequentialChain(df: pd.DataFrame) -> None:
+  llm = ChatOpenAI(temperature=0.9, model=llm_model)
 
-# prompt template 1: translate to english
-first_prompt = ChatPromptTemplate.from_template(
-  "다음 리뷰를 한국어로 번역하라:"
-  "\n\n{Review}"
-)
-# chain 1: input= Review and output= Korean_Review
-chain_one = LLMChain(llm=llm, prompt=first_prompt,
-           output_key="Korean_Review"
-          )
+  # prompt template 1: translate to english
+  first_prompt = ChatPromptTemplate.from_template(
+    "다음 리뷰를 한국어로 번역하라:"
+    "\n\n{Review}"
+  )
+  # chain 1: input= Review and output= Korean_Review
+  chain_one = LLMChain(llm=llm, prompt=first_prompt,
+            output_key="Korean_Review"
+            )
 
-second_prompt = ChatPromptTemplate.from_template(
-  "다음 리뷰를 1문장으로 요약하라:"
-  "\n\n{Korean_Review}"
-)
-# chain 2: input= Korean_Review and output= summary
-chain_two = LLMChain(llm=llm, prompt=second_prompt,
-           output_key="summary"
-          )
-
-
-# prompt template 3: translate to english
-third_prompt = ChatPromptTemplate.from_template(
-  "다음 리뷰의 언어는 무엇인가:\n\n{Review}"
-)
-# chain 3: input= Review and output= language
-chain_three = LLMChain(llm=llm, prompt=third_prompt,
-             output_key="language"
+  second_prompt = ChatPromptTemplate.from_template(
+    "다음 리뷰를 1문장으로 요약하라:"
+    "\n\n{Korean_Review}"
+  )
+  # chain 2: input= Korean_Review and output= summary
+  chain_two = LLMChain(llm=llm, prompt=second_prompt,
+            output_key="summary"
             )
 
 
-# prompt template 4: follow up message
-fourth_prompt = ChatPromptTemplate.from_template(
-  "다음의 요약 내용에 대해서 주어진 언어로 후속 답변을 작성하라:"
-  "\n\n요약: {summary}\n\n언어: {language}"
-)
-# chain 4: input= summary, language and output= followup_message
-chain_four = LLMChain(llm=llm, prompt=fourth_prompt,
-            output_key="followup_message"
-           )
+  # prompt template 3: translate to english
+  third_prompt = ChatPromptTemplate.from_template(
+    "다음 리뷰의 언어는 무엇인가:\n\n{Review}"
+  )
+  # chain 3: input= Review and output= language
+  chain_three = LLMChain(llm=llm, prompt=third_prompt,
+              output_key="language"
+              )
 
 
-# overall_chain: input= Review
-# and output= Korean_Review,summary, followup_message
-overall_chain = SequentialChain(
-  chains=[chain_one, chain_two, chain_three, chain_four],
-  input_variables=["Review"],
-  output_variables=["Korean_Review", "summary","followup_message"],
-  verbose=True
-)
+  # prompt template 4: follow up message
+  fourth_prompt = ChatPromptTemplate.from_template(
+    "다음의 요약 내용에 대해서 주어진 언어로 후속 답변을 작성하라:"
+    "\n\n요약: {summary}\n\n언어: {language}"
+  )
+  # chain 4: input= summary, language and output= followup_message
+  chain_four = LLMChain(llm=llm, prompt=fourth_prompt,
+              output_key="followup_message"
+            )
+
+  # overall_chain: input= Review
+  # and output= Korean_Review,summary, followup_message
+  overall_chain = SequentialChain(
+    chains=[chain_one, chain_two, chain_three, chain_four],
+    input_variables=["Review"],
+    output_variables=["Korean_Review", "summary","followup_message"],
+    verbose=True
+  )
+
+  review = df.Review[5]
+  response = overall_chain(review)
+  print(response)
+  return None
 
 
-review = df.Review[5]
-response = overall_chain(review)
-print(response)
+if __name__ == '__main__':
+  df: pd.DataFrame = pd.read_csv('data/Data.csv')
+  print(df.head())
 
+  test_LLMChain()
+  test_SimpleSequentialChain()
+  test_SequentialChain(df)
---
+++ langchain_8_document_loading.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Document Loading
+
+
+#! pip install langchain
+
+import os
+import time
+import json
+
+import openai
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("ORGANIZATION")
+
+
+# ## PDFs
+#! pip install pypdf
+
+from langchain.document_loaders import PyPDFLoader
+
+def test_pdf() -> None:
+  loader = PyPDFLoader("./data/프리랜서 가이드라인 (출판본).pdf")
+  pages = loader.load()
+  print(f"len(pages) => {len(pages)}")
+  page = pages[0]
+  print(page.page_content[0:500])
+  print( f"page.metadata => {page.metadata}" )
+  return None
+
+
+# ## YouTube
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+
+# ! pip install yt_dlp
+# ! pip install pydub
+
+def test_youtube() -> None:
+  url="https://www.youtube.com/watch?v=jGwO_UgTS7I"
+  save_dir="docs/youtube/"
+  loader = GenericLoader(
+    YoutubeAudioLoader([url],save_dir),
+    OpenAIWhisperParser()
+  )
+  docs = loader.load()
+  print( docs[0].page_content[0:500] )
+  return None
+
+
+# ## URLs
+
+from langchain.document_loaders import WebBaseLoader
+
+def test_url() -> None:
+  loader = WebBaseLoader("https://ko.wikipedia.org/wiki/NewJeans")
+  docs = loader.load()
+  print(docs[0].page_content[:500])
+  return None
+
+
+
+
